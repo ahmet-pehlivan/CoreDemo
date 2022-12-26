9cdeda03791d5bc36287a5b7d2d15b15960d80f6
Ahmet Pehlivan <115579325+ahmet-pehlivan@users.noreply.github.com>
2022-12-26 21:37:06 +0300

    26-12-22

---
+++ CoreDemo/ViewComponents/CommentList.cs
@@ -0,0 +1,31 @@
+﻿using CoreDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace CoreDemo.ViewComponents
+{
+	public class CommentList : ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			var CommentValues = new List<UserComment>
+			{
+				new UserComment
+				{
+					ID = 1,
+					Username = "Ahmet"
+				},
+				new UserComment
+				{
+					ID = 2,
+					Username = "Mert"
+				},
+				new UserComment
+				{
+					ID = 3,
+					Username = "Merve"
+				} }; 
+			return View(CommentValues); 
+		}
+	}
+}
