5ea6881e0bef31571b6b13e409d9a25055e6c658
Ahmet Pehlivan <115579325+ahmet-pehlivan@users.noreply.github.com>
2022-12-31 00:37:56 +0300

    31-12-22

---
+++ CoreDemo/Controllers/NewsLetterController.cs
@@ -0,0 +1,25 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.Controllers
+{
+	public class NewsLetterController : Controller
+	{
+		NewsLetterManager nm = new NewsLetterManager(new EfNewsLetterRepository());
+		[HttpGet]
+		public PartialViewResult SubscribeMail()
+		{
+			return PartialView();
+		}
+		[HttpPost]
+		public IActionResult SubscribeMail(NewsLetter p)
+		{
+			p.MailStatus=true;
+			nm.AddNewsLetter(p);
+			Response.Redirect("/Blog/BlogReadAll/"+1); //ajax ve jquery uygula
+			return PartialView();
+		}
+	}
+}
